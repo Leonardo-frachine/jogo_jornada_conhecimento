a69914c11ac2eacfc2bacfed2e15d0c9483d9232
Nathan Mariotto <115180122+Mariott0@users.noreply.github.com>
2026-08-20 17:21:45 -0300

    Atualiza mensagem inicial da API

--- jornada-api/src/app.service.ts
+++ jornada-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Api ta no ar bem vindo ao jornada do conhecimento para mais informações acesse o link: https://nathanmariotto.com.br/jornada';
   }
 }
